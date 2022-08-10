f771ac9099fba3b448444a66f0cfbde01b26df64
iamkimseunghyun <iamkimseunghyun@gmail.com>
2022-08-10 22:07:56 +0900

    정규 표현식 기본

---
+++ main.js
@@ -0,0 +1,12 @@
+let str = `
+010-1234-5678
+kaka@noridan.org
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+hxyp
+d
+동해물과_백두산이 마르고 닳도록
+`;
+
+console.log(str.match(/\b/g));
